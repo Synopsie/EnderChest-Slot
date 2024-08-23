a202ea8d1950e87d600647bbf3c6a7cc3729c8e8
Dumont-Julien <dumontj357@gmail.com>
2024-08-23 19:21:58 +0200

    possibilité de modifier de nom de l'enderchest.

--- src/listener/InventoryOpenListener.php
+++ src/listener/InventoryOpenListener.php
@@ -20,36 +20,58 @@ declare(strict_types=1);
 namespace slots\listener;
 
 use pocketmine\block\inventory\EnderChestInventory;
+use pocketmine\block\tile\EnderChest;
 use pocketmine\event\inventory\InventoryOpenEvent;
 use pocketmine\event\Listener;
 use pocketmine\item\StringToItemParser;
+use pocketmine\nbt\tag\StringTag;
+use pocketmine\network\mcpe\protocol\BlockActorDataPacket;
+use pocketmine\network\mcpe\protocol\types\BlockPosition;
+use pocketmine\network\mcpe\protocol\types\CacheableNbt;
 use slots\Main;
 use function count;
 
 class InventoryOpenListener implements Listener {
-	public function onInventoryOpen(InventoryOpenEvent $event) : void {
-		$inventory = $event->getInventory();
-		if($inventory instanceof EnderChestInventory) {
-			$player                = $event->getPlayer();
-			$enderchestPermissions = Main::getInstance()->getEnderChestSlotCache()->slots;
-			$enderchestSlotsCounts = 0;
-			foreach ($enderchestPermissions as $permissionName => $enderchestSlotInfo) {
-				if (!$player->hasPermission($permissionName)) {
-					continue;
-				}
-				$enderchestSlotsCounts = $enderchestSlotInfo->getSlots();
-			}
 
-			$config = Main::getInstance()->getConfig();
-			$item   = StringToItemParser::getInstance()->parse($config->getNested('item.id'))->setCustomName($config->getNested('item.name', '§o§cBloqué'));
+    public function onInventoryOpen(InventoryOpenEvent $event) : void {
+        $inventory = $event->getInventory();
+        if ($inventory instanceof EnderChestInventory) {
+            $player = $event->getPlayer();
+            $config = Main::getInstance()->getConfig();
+            $customNameFormat = str_replace('%player%', $player->getName(), $config->get('enderchest.name', '§8EnderChest de §e%player%'));
+            $customName = str_replace('%player%', $player->getName(), $customNameFormat);
 
-			$currentContents = $inventory->getContents();
-			$availableSlots  = $enderchestSlotsCounts - count($currentContents);
+            $tile = $inventory->getHolder()->getWorld()->getTile($inventory->getHolder());
+            if ($tile instanceof EnderChest) {
+                $nbt = $tile->saveNBT();
+                $nbt->setTag('CustomName', new StringTag($customName));
+                $packet = BlockActorDataPacket::create(
+                    new BlockPosition(
+                        (int)$tile->getPosition()->getX(),
+                        (int)$tile->getPosition()->getY(),
+                        (int)$tile->getPosition()->getZ()
+                    ),
+                    new CacheableNbt($nbt)
+                );
+                $player->getNetworkSession()->sendDataPacket($packet);
+            }
 
-			$totalSlots = $inventory->getSize();
-			for ($i = count($currentContents) + $availableSlots; $i < $totalSlots; $i++) {
-				$inventory->setItem($i, $item);
-			}
-		}
-	}
+            $enderchestPermissions = Main::getInstance()->getEnderChestSlotCache()->slots;
+            $enderchestSlotsCounts = 0;
+            foreach ($enderchestPermissions as $permissionName => $enderchestSlotInfo) {
+                if (!$player->hasPermission($permissionName)) {
+                    continue;
+                }
+                $enderchestSlotsCounts = $enderchestSlotInfo->getSlots();
+            }
+
+            $item = StringToItemParser::getInstance()->parse($config->getNested('item.id'))->setCustomName($config->getNested('item.name', '§o§cBloqué'));
+            $currentContents = $inventory->getContents();
+            $availableSlots = $enderchestSlotsCounts - count($currentContents);
+            $totalSlots = $inventory->getSize();
+            for ($i = count($currentContents) + $availableSlots; $i < $totalSlots; $i++) {
+                $inventory->setItem($i, $item);
+            }
+        }
+    }
 }
